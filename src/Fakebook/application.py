b5b54efc268216d89c0b108caa1f514a52441ee3
Brian Nguyen <44927554+the-brain711@users.noreply.github.com>
2023-10-28 17:14:10 -0500

    Setup AWS code pipeline for automatic deployment to Elastic Beanstalk (#21)
    Setup aws code deployment pipeline

---
+++ src/Fakebook/application.py
@@ -0,0 +1,16 @@
+from flask import Flask
+application = Flask(__name__) # This needs to be named `application`
+
+
+@application.route("/")
+def index():
+    return "<h1>Hello World!</h1>"
+
+
+@application.route("/another")
+def another():
+    return "<h1>Another Page!</h1>"
+
+
+if __name__ == "__main__":
+    application.run(debug=True)
