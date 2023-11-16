7d48a91bde7400cf2ce0d76725ae04d1d2965f9e
Brian Nguyen <briannguyen9101@gmail.com>
2023-11-16 13:43:15 -0600

    Fixed bug with timeline not showing some posts of friends

--- src/Fakebook/models/timeline.py
+++ src/Fakebook/models/timeline.py
@@ -13,8 +13,9 @@ class Timeline:
     def view_timeline(self):
         cursor = self.cursor
         cursor.execute(
-            "SELECT posts_tb.* FROM posts_tb WHERE posts_tb.user_id = %s OR posts_tb.user_id IN (SELECT friend_accepter_id FROM friends_tb WHERE friend_requester_id = %s) ORDER BY posts_tb.creation_date DESC",
+            "SELECT * FROM (SELECT * FROM posts_tb WHERE user_id = %s UNION SELECT posts_tb.* FROM posts_tb INNER JOIN friends_tb ON posts_tb.user_id = friends_tb.friend_requester_id WHERE friends_tb.friend_accepter_id = %s AND friends_tb.friend_request_status = 'ACCEPTED' UNION SELECT posts_tb.* FROM posts_tb INNER JOIN friends_tb ON posts_tb.user_id = friends_tb.friend_accepter_id WHERE friends_tb.friend_requester_id = %s AND friends_tb.friend_request_status = 'ACCEPTED') AS combined_result ORDER BY combined_result.creation_date DESC",
             (
+                self.user_id,
                 self.user_id,
                 self.user_id,
             ),
